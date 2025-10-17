c676c385a867195f56cde699988279534ddae082
ilxp <lixp@live.com>
2025-10-18 03:38:06 +0800

    Update build-oprx.sh

--- openwrt/build-oprx.sh
+++ openwrt/build-oprx.sh
@@ -226,7 +226,7 @@ else
 fi
 cat > feeds.conf <<EOF
 #src-git packages https://$github/openwrt/packages.git$packages
-src-git https://github.com/coolsnowwolf/packages;master
+src-git packages https://github.com/coolsnowwolf/packages;master
 src-git luci https://$github/openwrt/luci.git$luci
 src-git routing https://$github/openwrt/routing.git$routing
 src-git telephony https://$github/openwrt/telephony.git$telephony
