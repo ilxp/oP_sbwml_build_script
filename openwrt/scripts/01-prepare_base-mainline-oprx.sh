8aece5dabb565f69f1fade043660ee544a67d5ac
ilxp <lixp@live.com>
2025-10-18 05:37:36 +0800

    Update 01-prepare_base-mainline-oprx.sh

--- openwrt/scripts/01-prepare_base-mainline-oprx.sh
+++ openwrt/scripts/01-prepare_base-mainline-oprx.sh
@@ -138,43 +138,43 @@ merge_package linux-6.6 https://github.com/mj22226/openwrt.git  target/linux/x86
 [ "$platform" = "bcm53xx" ] && [ "$KERNEL_CLANG_LTO" = "y" ] && rm -f target/linux/generic/hack-6.6/220-arm-gc_sections.patch target/linux/generic/hack-6.12/220-arm-gc_sections.patch
 
 # kernel modules
-#rm -rf package/kernel/linux
-#git checkout package/kernel/linux
-#pushd package/kernel/linux/modules
-    #rm -f [a-z]*.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/block.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/can.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/crypto.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/firewire.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/fs.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/gpio.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/hwmon.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/i2c.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/iio.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/input.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/leds.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/lib.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/multiplexer.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netdevices.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netfilter.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netsupport.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/nls.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/other.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/pcmcia.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/rtc.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/sound.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/spi.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/usb.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/video.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/virt.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/w1.mk
-    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/wpan.mk
-#popd
-
 rm -rf package/kernel/linux
-merge_package linux-6.6 https://github.com/mj22226/openwrt.git  package/kernel package/kernel/linux
-rm -rf package/kernel/linux/modules/netsupport.mk
-curl -s https://github.com/sbwml/r4s_build_script/raw/master/openwrt/patch/openwrt-6.x/modules/netsupport.mk > package/kernel/linux/modules/netsupport.mk
+git checkout package/kernel/linux
+pushd package/kernel/linux/modules
+    rm -f [a-z]*.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/block.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/can.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/crypto.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/firewire.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/fs.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/gpio.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/hwmon.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/i2c.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/iio.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/input.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/leds.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/lib.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/multiplexer.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netdevices.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netfilter.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netsupport.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/nls.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/other.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/pcmcia.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/rtc.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/sound.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/spi.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/usb.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/video.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/virt.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/w1.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/wpan.mk
+popd
+
+#rm -rf package/kernel/linux
+#merge_package linux-6.6 https://github.com/mj22226/openwrt.git  package/kernel package/kernel/linux
+#rm -rf package/kernel/linux/modules/netsupport.mk
+#curl -s https://github.com/sbwml/r4s_build_script/raw/master/openwrt/patch/openwrt-6.x/modules/netsupport.mk > package/kernel/linux/modules/netsupport.mk
 
 # BBRv3 - linux-6.12
 pushd target/linux/generic/backport-6.12
