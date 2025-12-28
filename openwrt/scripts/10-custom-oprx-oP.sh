989596166dfb9cbc827851edbd18f3a40421b78f
ilxp <lixp@live.com>
2025-12-28 10:30:26 +0800

    Update 10-custom-oprx-oP.sh

--- openwrt/scripts/10-custom-oprx-oP.sh
+++ openwrt/scripts/10-custom-oprx-oP.sh
@@ -831,9 +831,6 @@ git clone -b openwrt-24.10 --depth 1 https://github.com/sbwml/autocore-arm  pack
 sed -i '/init/d' package/system/autocore/Makefile
 sed -i '/autocore.json/a\\	$(INSTALL_BIN) ./files/x86/autocore $(1)/etc/init.d/' package/system/autocore/Makefile
 sed -i '/autocore.json/a\\	$(INSTALL_DIR) $(1)/etc/init.d' package/system/autocore/Makefile
-
-rm -rf package/system/autocore/files/x86/autocore  ##sbwml的cpu使用率有问题。采用yaof的
-merge_package master https://github.com/QiuSimons/OpenWrt-Add.git  package/system/autocore/files/x86 autocore
 sed -i 's/png/svg/g' package/system/autocore/files/generic/29_ports.js
 
 #采用immortalwrt的
