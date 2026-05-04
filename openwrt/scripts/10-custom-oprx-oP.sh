2c73ac316e383f0e97e60fd2c9f2ab5a3cbc7001
ilxp <lixp@live.com>
2026-05-04 09:14:38 +0800

    up

--- openwrt/scripts/10-custom-oprx-oP.sh
+++ openwrt/scripts/10-custom-oprx-oP.sh
@@ -633,15 +633,15 @@ git clone -b main --depth 1 https://github.com/ilxp/openwrt-control  package/diy
 #merge_package master https://github.com/xiangfeidexiaohuo/openwrt-packages.git package/new patch/luci-app-turboacc
 
 #适配 firewall4
-merge_package luci https://github.com/chenmozhijin/turboacc.git package/new/turboacc luci-app-turboacc
+#merge_package luci https://github.com/chenmozhijin/turboacc.git package/new/turboacc luci-app-turboacc
 #修改 bbr为bbr3
-sed -i 's/kmod-tcp-bbr/kmod-tcp-bbr3/g' package/new/turboacc/luci-app-turboacc/Makefile
+#sed -i 's/kmod-tcp-bbr/kmod-tcp-bbr3/g' package/new/turboacc/luci-app-turboacc/Makefile
 
 #适lede的luci23.05已经适配 firewall4，#lede的在oP上安装不上
-#merge_package openwrt-24.10 https://github.com/coolsnowwolf/luci.git package/new applications/luci-app-turboacc
-#sed -i 's/kmod-ipt-offload/kmod-nft-offload /g' package/new/luci-app-turboacc/Makefile
+merge_package openwrt-25.12 https://github.com/coolsnowwolf/luci.git package/new applications/luci-app-turboacc
+sed -i 's/kmod-ipt-offload/kmod-nft-offload /g' package/new/luci-app-turboacc/Makefile
 #修改 bbr为bbr3
-#sed -i 's/kmod-tcp-bbr/kmod-tcp-bbr3/g' package/new/luci-app-turboacc/Makefile
+sed -i 's/kmod-tcp-bbr/kmod-tcp-bbr3/g' package/new/luci-app-turboacc/Makefile
 
 
 #2、京东签到 By Jerrykuku 作者已关闭了
