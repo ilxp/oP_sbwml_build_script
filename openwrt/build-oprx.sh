5e9dcab0a26f8427ad98653811eb315ae8bfeea8
ilxp <lixp@live.com>
2026-09-18 23:24:49 +0800

    Update build-oprx.sh

--- openwrt/build-oprx.sh
+++ openwrt/build-oprx.sh
@@ -310,13 +310,13 @@ fi
 
 # config-common
 if [ "$MINIMAL_BUILD" = "y" ]; then
-    curl -s $mirror/openwrt/25-config-minimal-common >> .config
+    curl -s $mirror/openwrt/configs/25-config-minimal-common >> .config
     echo 'VERSION_TYPE="minimal"' >> package/base-files/files/usr/lib/os-release
 elif [ "$STD_BUILD" = "y" ]; then
-    curl -s $mirror/openwrt/25-config-std-common >> .config
+    curl -s $mirror/openwrt/configs/25-config-std-common >> .config
     echo 'VERSION_TYPE="standard"' >> package/base-files/files/usr/lib/os-release
 else
-    curl -s $mirror/openwrt/25-config-common-oprx >> .config
+    curl -s $mirror/openwrt/configs/25-config-common-oprx >> .config
     [ "$platform" = "armv8" ] && sed -i '/DOCKER/Id' .config
 fi
 
